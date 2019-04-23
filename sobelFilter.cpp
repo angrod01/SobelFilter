d2c52a317ac6fc950bacca56eaa9865161d56f0f
angrod01 <35838152+angrod01@users.noreply.github.com>
2019-04-23 15:42:45 +0200

    Implemented transformation function with dummy pixel gradient values

--- sobelFilter.cpp
+++ sobelFilter.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -15,7 +17,19 @@ int yGradient (cv::Mat image, int i, int j){
 cv::Mat sobelTransform(const cv::Mat& inputImage)
 {
     cv::Mat filteredImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
-    //TODO
+    
+    for (int j = 0; j < inputImage.rows-2;  ++j) {
+        for (int i = 0;  i < inputImage.cols-2;  ++i) {
+            // Calculate X gradient of pixel
+            int xValOfPixel = xGradient(inputImage, i, j);
+            // Calculate Y gradient of pixel
+            int yValOfPixel = yGradient(inputImage, i, j);
+            // Calculate magnitude (absolute aproximation)
+            int sum = std::clamp(std::abs(xValOfPixel) + std::abs(yValOfPixel), 0, 255);
+
+            filteredImage.at<uchar>(j, i) = (uchar)sum;
+        }
+    }
 
     return filteredImage;
 }
@@ -23,5 +37,7 @@ cv::Mat sobelTransform(const cv::Mat& inputImage)
 
 int main(int argc, char** argv)
 {
-    //TODO
+    const cv::Mat initialImage = cv::imread("Cats.jpg", cv::IMREAD_GRAYSCALE);
+    const cv::Mat finalImage = sobelTransform(initialImage);
+    cv::imwrite("CatsBorders.jpg",finalImage);
 }
