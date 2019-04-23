97877e0c80784f7573e341fbf07f77793d920b8a
angrod01 <35838152+angrod01@users.noreply.github.com>
2019-04-23 15:27:57 +0200

    Created skeleton of functions for the Sobel filter

---
+++ sobelFilter.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+
+int xGradient(cv::Mat image, int i, int j){
+    //TODO
+    return  0;
+
+}
+int yGradient (cv::Mat image, int i, int j){
+    //TODO
+    return 0;
+}
+
+cv::Mat sobelTransform(const cv::Mat& inputImage)
+{
+    cv::Mat filteredImage = cv::Mat::zeros(inputImage.size(), inputImage.type());
+    //TODO
+
+    return filteredImage;
+}
+
+
+int main(int argc, char** argv)
+{
+    //TODO
+}
